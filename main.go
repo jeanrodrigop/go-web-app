a823068226ee3a92d1b39e4f4e351117e6988d91
Jean Rodrigo <jeanrodrigo_p@outlook.com>
2024-08-31 19:26:09 +0000

    Modularizando o codigo

--- main.go
+++ main.go
@@ -1,29 +1,10 @@
 package main
 
 import (
-	"database/sql"
+	"go-web-app/models"
 	"html/template"
 	"net/http"
-
-	_ "github.com/lib/pq"
 )
-
-func conectaBancoDeDados() *sql.DB {
-	conexao := "user=postgres dbname=alura_loja password=passw0rd host=localhost sslmode=disable"
-	db, err := sql.Open("postgres", conexao)
-	if err != nil {
-		panic(err.Error())
-	}
-	return db
-}
-
-type Produto struct {
-	Id         int
-	Nome       string
-	Descricao  string
-	Preco      float64
-	Quantidade int
-}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
@@ -33,34 +14,6 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	db := conectaBancoDeDados()
-
-	selectTodosProdutos, err := db.Query("select * from produtos")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p := Produto{}
-	produtos := []Produto{}
-
-	for selectTodosProdutos.Next() {
-		var id, quantidade int
-		var nome, descricao string
-		var preco float64
-
-		err = selectTodosProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		p.Nome = nome
-		p.Descricao = descricao
-		p.Preco = preco
-		p.Quantidade = quantidade
-
-		produtos = append(produtos, p)
-	}
-
-	temp.ExecuteTemplate(w, "Index", produtos)
-	defer db.Close()
+	todosProdutos := models.BuscaTodosProdutos()
+	temp.ExecuteTemplate(w, "Index", todosProdutos)
 }
