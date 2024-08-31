e0127fe9e7f815af78ed7c59195c956aae92e47c
Jean Rodrigo <jeanrodrigo_p@outlook.com>
2024-08-31 22:28:06 +0000

    Refatorando a pagina de novos produtos

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"go-web-app/models"
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
@@ -11,4 +13,30 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 func Index(w http.ResponseWriter, r *http.Request) {
 	todosProdutos := models.BuscaTodosProdutos()
 	temp.ExecuteTemplate(w, "Index", todosProdutos)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertidoFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço:", err)
+		}
+
+		quantidadeConvertidaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão do quantidade:", err)
+		}
+
+		models.CriaNovoProduto(nome, descricao, precoConvertidoFloat, quantidadeConvertidaInt)
+	}
+	http.Redirect(w, r, "/", 301)
 }
